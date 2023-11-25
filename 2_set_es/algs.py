07e83f62b8e09d9b44ed84973473c7f02da34361
ShoT <michaelcavicchioli.informatica@gmail.com>
2023-11-25 17:29:30 +0100

    .

--- 2_set_es/algs.py
+++ 2_set_es/algs.py
@@ -1,6 +1,7 @@
 import random, time
+import numpy as np
 
-def euclide_esteso(a, b) -> int:
+def euclide_esteso(a, b):
     # caso base in cui a = 0 e b = 1
     if a % b == 0: return (b, 0, 1) 
     else:
@@ -33,7 +34,7 @@ def esponenziazione_modulare_veloce(a, e, m) -> int:
 
 print(esponenziazione_modulare_veloce(3, 11, 10))
 
-def trova_m_dispari_e_r(n_1, r):
+def trova_m_dispari_e_r(n_1, r = 0):
     if n_1 % 2 != 0: return n_1, r
     n_1, r = trova_m_dispari_e_r(n_1//2, r + 1)
     return n_1, r
@@ -46,7 +47,7 @@ def miller_rabin(n, x = 2) -> bool:
     if n % 2 == 0: return True
 
     n_1 = n-1
-    m, r = trova_m_dispari_e_r(n_1, 0)    
+    m, r = trova_m_dispari_e_r(n_1)    
 
     x = esponenziazione_modulare_veloce(x, m, n)
 
@@ -103,8 +104,14 @@ def generatore_numeri_primi(k) -> list:
 
 print("Il numero primo è:", generatore_numeri_primi(200))
 
-def rsa_decryption(ct, d, n):
+def rsa_decryption_normale(ct, d, n):
     return esponenziazione_modulare_veloce(ct, d, n)
+
+def rsa_decryption_crt(p, q, sp, sq, n):
+    q_inverso = euclide_esteso(q, p)[1]
+    p_inverso = euclide_esteso(p, q)[1]
+    
+    return ((q*(q_inverso % p)*sp + p*(p_inverso % q)*sq) % n)
 
 def rsa():
     # generazione di p, q
@@ -117,27 +124,147 @@ def rsa():
     # calcolo phi(n)
     phi_n = (p-1) * (q-1)
 
-    # genero 'd' ed 'e'
+    # genero 'd' (di circa 200 bit) ed 'e'
     while True:
-        d = generatore_numeri_primi(200)
-        e = euclide_esteso(d, phi_n)[1]
-        if euclide_esteso(d, phi_n)[0] == 1: break
+        # considero i bit di 'n' e li divido per 8 per ottenere il numero di byte
+        # genero randomicamente quei byte, mettendo il valore più sigificativo
+        # in prima posizione (più a sx) e riporto il risultato in bit
+        d = int.from_bytes(random.randbytes(int.bit_count(n)//8), "big")
+        # d*e congruo 1 mod phi_n
+        mcd, e, _ = euclide_esteso(d, phi_n) 
+        if mcd == 1: break
 
     # se negativa => calcolo il modulo 
-    if e < 0:
-        e %= phi_n
+    if e < 0: e %= phi_n
 
     # scambio i valori di 'e' con 'd' e 'd' con 'e' 
     # per avere 'd' > 'e', altrimenti l'attaccante
     # potrebbe trovare 'd' per tentativi
     if e > d: e, d = d, e
 
+    '''
+    print("N", int.bit_count(n))
+    print("q", int.bit_count(d))
+    print(len(str(p)))
+    print(len(str(q)))
+    print(len(str(n)))
+    print(len(str(d)))
+    print(len(str(e)))
+    '''
+    
     # genero 100 ct, randomicamente, di lunghezza 100 
-    for i in range(100):
-        ct = random.randrange(1, 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
-        inizio = time.time()
-        pt = rsa_decryption(ct, d, n)
-        fine = time.time()
-        print(fine-inizio)        
+    iterazioni = 100
+    norm_maggiore_crt = 0
+    while iterazioni > 0:
+        ct = random.randrange(1, 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)        
+
+        '''
+        print("ct", int.bit_count(ct))
+        print("r1crt", int.bit_count(resto1_crt))
+        print("r2crt", int.bit_count(resto2_crt))
+        '''
+        inizio_decr_norm = time.time()
+        uno = rsa_decryption_normale(ct, d, n)
+        fine_decr_norm = time.time()
+        
+        sp = esponenziazione_modulare_veloce(ct, d, p)
+        sq = esponenziazione_modulare_veloce(ct, d, q)
+        inizio_decr_crt = time.time()
+        due = rsa_decryption_crt(p, q, sp, sq, n)
+        fine_decr_crt = time.time()
+
+        diff_norm = fine_decr_norm - inizio_decr_norm
+        diff_crt = fine_decr_crt - inizio_decr_crt
+        print("Norm", diff_norm)
+        print("Crt", diff_crt)
+
+        if diff_crt < diff_norm: norm_maggiore_crt += 1   
+        iterazioni -= 1        
+    
+    print(norm_maggiore_crt)
 
 rsa()
+
+
+
+
+
+
+
+# True = Composto 
+# False = forse primo
+def miller_rabin_modificato(n, m, r, x = 2) -> bool:
+    x = esponenziazione_modulare_veloce(x, m, n)
+
+    # x banale
+    if x == 1 or x == -1: return -1
+
+    while r > 0:
+        # x banale
+        if ((x % n) - n) == -1: return -1
+
+        x_precedente = x
+        x = esponenziazione_modulare_veloce(x, 2, n)
+
+        if x == 1 and (x_precedente % n != - 1):
+            return euclide_esteso(x_precedente + 1, n)[0]
+
+        r -= 1
+
+    return -1
+
+def decryptionexp(n, d, e):
+    iterazioni = 0
+    # 1) ed - 1 = 2^r * m 
+    m, r = trova_m_dispari_e_r((e*d-1))
+
+    # 2) x, random, coprimo con n
+    while True:
+        x = random.randint(2, n-1)
+
+        # mcd != 1 => ho trovato l'mcd
+        if euclide_esteso(x, n)[0] != 1:
+            return 0
+
+        iterazioni += 1
+        # Miller-Rabin     
+        mcd = miller_rabin_modificato(n, m, r, x)
+        if mcd != -1: return iterazioni
+
+def rsa_3_2():
+    tempo_esecuzione_algoritmo = [0.0] * 100
+    numero_iterazioni_algoritmo = [0] * 100
+
+    for i in range(100):
+        # generazione di p, q
+        while True: 
+            p, q = generatore_numeri_primi(200), generatore_numeri_primi(200)
+            if p != q: break
+            
+        # calcolo n
+        n = p*q
+        # calcolo phi(n)
+        phi_n = (p-1) * (q-1)
+
+        # genero 'd' (di circa 200 bit) ed 'e'
+        while True:
+            # considero i bit di 'n' e li divido per 8 per ottenere il numero di byte
+            # genero randomicamente quei byte, mettendo il valore più sigificativo
+            # in prima posizione (più a sx) e riporto il risultato in bit
+            d = int.from_bytes(random.randbytes(int.bit_count(n)//8), "big")
+            # d*e congruo 1 mod phi_n
+            mcd, e, _ = euclide_esteso(d, phi_n)
+            if mcd == 1: break
+
+        inizio = time.time()
+        iterazioni_algoritmo =  decryptionexp(n, d, e)
+        fine = time.time()
+
+        numero_iterazioni_algoritmo[i] = iterazioni_algoritmo
+        tempo_esecuzione_algoritmo[i] = fine - inizio
+
+    print("# medio iterazioni:", str(sum(numero_iterazioni_algoritmo) / 100))
+    print("Tempo medio esecuzione (secondi):", str(sum(tempo_esecuzione_algoritmo) / 100))
+    print("Varianza (s^2):", np.var(tempo_esecuzione_algoritmo))
+
+rsa_3_2()
--- 2_set_es/common_modulus_failure.py
+++ 2_set_es/common_modulus_failure.py
@@ -10,6 +10,23 @@ def euclide_esteso(a, b) -> int:
 
     return mcd, x, c
 
+def esponenziazione_modulare_veloce(a, e, m) -> int:
+    # l'esponente lo converto in base 2 ed elimino i primi
+    # due caratteri (0b)
+    e = bin(e)[2:]
+    d = 1
+    c = 0
+    
+    for i in e:
+        d = (d * d) % m
+        c *= 2
+        
+        if i == '1':
+            d = (d * a) % m
+            c += 1
+    
+    return d
+
 n = 1309914994772590863210166992356557234456075980579048604758768205496404269677841156459642052158879494989630338961154043468325508153199153204245943547981
 
 c1 = 1208833588708967444709375
@@ -17,10 +34,16 @@ c2 = 411294544478239271886338859092185183748200324266700081787109375
 
 e1, e2 = 5, 13
 
+# e1*x + e2*y = 1
 _, x, y = euclide_esteso(e1, e2)
 
-_, _, c1_inverse = euclide_esteso(c1, n)
+# c1*c1_inverso congruo 1 mod n
+_, c1_inverso, _ = euclide_esteso(c1, n)
 
-m = ((c1**x) * (c2**y)) % n
+# abs(x) = prendo il valore assoluto, quindi positivo
+m = (
+    esponenziazione_modulare_veloce(c1_inverso, abs(x), n) * 
+    esponenziazione_modulare_veloce(c2, y, n)
+    ) % n
 
 print("Il messaggio è:", m)
